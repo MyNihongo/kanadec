6afd81b12536c4098a3fec1a94ff34184c4dd593
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2021-11-11 21:15:23 +0200

    add has checks

--- string_extensions.go
+++ string_extensions.go
@@ -30,6 +30,36 @@ func IsRomaji(value string) bool {
 	return isCheck(value, IsRuneRomaji)
 }
 
+// HasHiragana checks whether the provided value has a valid hiragana character
+func HasHiragana(value string) bool {
+	return hasCheck(value, IsRuneHiragana)
+}
+
+// HasKatakana checks whether the provided value has a valid katakana character
+func HasKatakana(value string) bool {
+	return hasCheck(value, IsRuneKatakana)
+}
+
+// HasKana checks whether the provided value has a valid hiragana or katakana character
+func HasKana(value string) bool {
+	return hasCheck(value, IsRuneKana)
+}
+
+// HasKanji checks whether the provided value has a valid kanji character
+func HasKanji(value string) bool {
+	return hasCheck(value, IsRuneKanji)
+}
+
+// HasKanaOrKanji checks whether the provided value has a valid hiragana, katakana or kanji character
+func HasKanaOrKanji(value string) bool {
+	return hasCheck(value, IsRuneKanaOrKanji)
+}
+
+// HasRomaji checks whether the provided value has a valid romaji character
+func HasRomaji(value string) bool {
+	return hasCheck(value, IsRuneRomaji)
+}
+
 func isCheck(value string, check func(rune) bool) bool {
 	for _, r := range value {
 		if !check(r) {
@@ -38,4 +68,14 @@ func isCheck(value string, check func(rune) bool) bool {
 	}
 
 	return true
+}
+
+func hasCheck(value string, check func(rune) bool) bool {
+	for _, r := range value {
+		if check(r) {
+			return true
+		}
+	}
+
+	return false
 }
